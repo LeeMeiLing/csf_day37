170682fa42771a975e6e5a0fa57ef8975cae1a85
LeeMeiLing <leem0060@outlook.com>
2023-05-03 14:44:06 +0800

    initial

---
+++ server/src/main/java/sg/edu/nus/iss/day37_springboot/Day37SpringbootApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.day37_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day37SpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day37SpringbootApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/sg/edu/nus/iss/day37_springboot/Day37SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.day37_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day37SpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
